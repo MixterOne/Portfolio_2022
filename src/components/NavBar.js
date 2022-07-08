3dc08133df7ad5d9fb327e270e13a8df85f640a2
MixterOne <matheusisaias879@gmail.com>
2022-07-08 15:44:32 -0300

    iniciando projeto e adicionando navBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
+const NavBar = () => {
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true)
+            } else {
+                setScrolled(false)
+            }
+        }
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [])
+
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value)
+    }
+
+    return (  
+        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+            <Container>
+                <Navbar.Brand href="#home">
+                    <h1 style={{color: '#fff', textTransform: 'uppercase'}}>Matheus</h1>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className='navbar-toggle-icon'>Menu</span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                    <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                    <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                </Nav>
+                <span className="navbar-text">
+                    <div className="social-icon">
+                        <a href="https://www.linkedin.com/in/matheus-rodrigues-isaias/"><img src={navIcon1} alt="" /></a>
+                        <a href="https://github.com/MixterOne"><img src={navIcon2} alt="" /></a>
+                        <a href="https://wa.me/+5531988952080"><img src={navIcon3} alt="" /></a>
+                    </div>
+                    <button className="vvd" onClick={() => console.log("teste")}>
+                        <span>Let´s Connect</span>
+                    </button>
+                </span>
+                
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+ 
+export default NavBar;
